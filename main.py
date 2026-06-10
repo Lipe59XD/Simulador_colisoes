a944bfbeddd4a124a2f0dc5908d6b5ed89b8aac5
Lipe59XD <109003835+Lipe59XD@users.noreply.github.com>
2026-06-10 12:09:20 -0300

    Implement ball collision and movement simulation

---
+++ main.py
@@ -0,0 +1,112 @@
+import pygame as pg
+import random
+
+
+class Bola():
+    def __init__(self, pos_x, pos_y): #velocidades e posições aleatórias
+        self.velocity = pg.math.Vector2(random.randint(100, 200), random.randint(100, 200))
+        self.pos = pg.math.Vector2(pos_x, pos_y)
+    def update(self, dt): #atualizar a posição do objeto com base na velocidade
+        self.pos += self.velocity * dt
+        # Verificar colisão com as bordas da tela
+        if self.pos.x < radius or self.pos.x > screen_x-radius:
+            self.velocity.x *= -1
+        if self.pos.y < radius or self.pos.y > screen_y-radius:
+            self.velocity.y *= -1
+
+def is_colliding(obj1, obj2, radius=20):
+    distance = obj1.pos.distance_to(obj2.pos)
+    return distance < radius * 2
+
+def collide(obj1, obj2, c_restitution=1):
+    #Vetor direção entre os centros
+    p = obj1.pos - obj2.pos
+    if p.length_squared() == 0: 
+        return # Evita divisão por zero se estiverem no mesmo pixel
+    p.normalize_ip()
+
+    #separa a velocidade original na componente da colisão
+    v1_n = obj1.velocity.project(p)
+    v2_n = obj2.velocity.project(p)
+
+    #guarda a velocidade perpendicular que nn muda na batida
+    v1_t = obj1.velocity - v1_n
+    v2_t = obj2.velocity - v2_n
+
+    #centro de massa
+    v_cm_n = (v1_n + v2_n) / 2
+
+    #Aplica a fórmula apenas na componente da colisão
+    v1_n_final = (1 + c_restitution) * v_cm_n - c_restitution * v1_n
+    v2_n_final = (1 + c_restitution) * v_cm_n - c_restitution * v2_n
+
+    #A velocidade final
+    obj1.velocity = v1_n_final + v1_t
+    obj2.velocity = v2_n_final + v2_t
+
+#dimnesões da tela
+screen_x = 800
+screen_y = 600
+
+#raio das bolas
+radius = 50
+
+#coeficiente de restituicao
+c_restituition = 1
+
+#quantidade de bolas
+n_objects = 10
+objects = []
+for i in range(n_objects):
+    posicao_valida = False
+    tentativas = 0
+    max_tentativas = 1000  #tentativas maximas para n travar o jogo
+    
+    while not posicao_valida and tentativas < max_tentativas:
+        tentativas += 1
+        
+        # posição X e Y 
+        x_sorteado = random.randint(radius, screen_x - radius)
+        y_sorteado = random.randint(radius, screen_y - radius)
+        colidiu = False
+        novo_ponto = pg.math.Vector2(x_sorteado, y_sorteado)
+        
+        # varre lista 'objects' procurando colisões
+        for obj in objects:
+            distancia = novo_ponto.distance_to(obj.pos)
+            
+            # se a distância entre os centros for menor que a soma dos raios
+            if distancia < (radius + radius):
+                colidiu = True
+                break 
+                
+        if not colidiu:
+            posicao_valida = True
+            
+    # achou uma posição livre
+    if posicao_valida:
+        objects.append(Bola(x_sorteado, y_sorteado))
+    else:
+        print(f"Aviso: A tela ficou cheia. Foram criados apenas {len(objects)} objetos.")
+        break
+
+pg.init()
+clock = pg.time.Clock()
+screen = pg.display.set_mode((screen_x, screen_y))
+run = True
+while run:
+    dt = clock.tick(180)/1000.0
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+    for i in range(len(objects)):
+        for j in range(i+1, len(objects)):
+            if is_colliding(objects[i], objects[j], radius):
+                collide(objects[i], objects[j], c_restituition)
+        objects[i].update(dt)
+        pg.draw.circle(screen, (255, 255, 255), (float(objects[i].pos.x), float(objects[i].pos.y)), radius)
+    pg.display.flip()
+    screen.fill((0, 0, 0))
+
+
+pg.quit()
